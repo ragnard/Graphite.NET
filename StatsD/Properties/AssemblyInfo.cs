58e5603459003b143631127182ab9ef8902797ac
Ragnar Dahlen <r.dahlen@gmail.com>
2011-09-24 00:41:20 +0200

    Create nuspec package

--- StatsD/Properties/AssemblyInfo.cs
+++ StatsD/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StatsD")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("StatsD client library for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("StatsD")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Ragnar Dahlén")]
+[assembly: AssemblyProduct("StatsD.NET")]
+[assembly: AssemblyCopyright("Copyright © Ragnar Dahlén")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
